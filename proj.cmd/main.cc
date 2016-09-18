03cd9ff027c3fe37cfe547cc71ef295836f84759
FrankLin <lin.xiaoe.f@gmail.com>
2016-09-18 16:39:28 +0800

    Add some test.

--- proj.cmd/main.cc
+++ proj.cmd/main.cc
@@ -59,9 +59,9 @@ void list_files_do_stuff(const char* name, int level, const std::string& input_p
   if (!(entry = readdir(dir)))
     return;
   
-  gago::Logger::LogD("!!! - %s is %d", entry->d_name, entry->d_type);
-  
   do {
+    gago::Logger::LogD("!!! - %s is %d", entry->d_name, entry->d_type);
+    
     if (entry->d_type == DT_DIR) {
       gago::Logger::LogD("DEBUG - step2/ DIR - working %s", entry->d_name);
       
